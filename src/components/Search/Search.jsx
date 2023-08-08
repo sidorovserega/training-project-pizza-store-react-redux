bf560e698d9c95c2807da821f4a26685ad6616da
SergeySidorov <sidorovserega92@gmail.com>
2023-08-08 15:06:51 +0300

    создание отложенного запроса на сервер при поиске по названию

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,32 +1,47 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import styles from './Search.module.scss';
 import SVGGenerate from '../../assets/SVGGenerate/SVGGenerate';
+import debounce from 'lodash.debounce';
 
-const Search = ({searchName, onChangeSearch}) => {
+const Search = ({ searchName, onChangeSearch }) => {
   
-  const inputSearchRef = useRef();
+  const [valueInput, setValueInput] = React.useState(searchName);
+  const inputSearchRef = React.useRef();
 
   const onClickClear = () => {
     onChangeSearch('');
+    setValueInput('');
     inputSearchRef.current.focus();
-  }
+  };
+  //ссылка на функцию, которая отработает с задержкой,
+  //после чего отправит изменения в state и произойдет поиск
+  //необходима для уменьшения запросов на сервер при вводе в input 
+  const updateSearchName = React.useCallback(
+    debounce((str) => onChangeSearch(str), 
+    250), 
+    []
+  );
+
+  const onChangeInput = (event) => {
+    setValueInput(event.target.value);
+    updateSearchName(event.target.value);
+  };
 
   return (
     <div className={styles.root}>
-      <SVGGenerate nameSVG='iconSearch' className={styles.iconSearch}/>
-      <input 
+      <SVGGenerate nameSVG="iconSearch" className={styles.iconSearch} />
+      <input
         ref={inputSearchRef}
-        className={styles.searchInput} 
-        placeholder='Поиск пиццы ...'
-        value={searchName}  
-        onChange={(event) => onChangeSearch(event.target.value)}
+        className={styles.searchInput}
+        placeholder="Поиск пиццы ..."
+        value={valueInput}
+        onChange={onChangeInput}
       />
       <div onClick={onClickClear}>
-        <SVGGenerate nameSVG='iconClose' className={styles.iconClose} />
+        <SVGGenerate nameSVG="iconClose" className={styles.iconClose} />
       </div>
-      
     </div>
-  )
-}
+  );
+};
 
 export default Search;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react';
 import { Categories, SortPopup, PizzaBlock, PizzaLoadingBlock } from '../components/index';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchPizzas } from '../redux/actions/pizzas';
@@ -6,20 +6,20 @@ import { setCategory, setPageActive, setSortBy } from '../redux/actions/filters'
 import { addPizzaToBasket } from '../redux/actions/basket';
 import Pagination from '../components/pagination/Pagination';
 
-
 const Home = () => {
-
-  const {items, sortBy, category, searchName, pageActive, isLoading, basketItems} = useSelector(({pizzas, filters, basket}) => {
-    return {
-      items: pizzas.items,
-      isLoading: pizzas.isLoading,
-      sortBy: filters.sortBy,
-      category: filters.category,
-      searchName: filters.searchName,
-      pageActive: filters.pageActive,
-      basketItems: basket.items
-    }
-  });
+  const { items, sortBy, category, searchName, pageActive, isLoading, basketItems } = useSelector(
+    ({ pizzas, filters, basket }) => {
+      return {
+        items: pizzas.items,
+        isLoading: pizzas.isLoading,
+        sortBy: filters.sortBy,
+        category: filters.category,
+        searchName: filters.searchName,
+        pageActive: filters.pageActive,
+        basketItems: basket.items,
+      };
+    },
+  );
 
   const dispatch = useDispatch();
 
@@ -31,54 +31,62 @@ const Home = () => {
 
   const setActiveCategory = (index) => {
     dispatch(setCategory(index));
-  }
+  };
 
   const setActiveSortBy = (typeName) => {
     dispatch(setSortBy(typeName));
-  }
+  };
 
   const onClickAddPizza = (objPizza) => {
     dispatch(addPizzaToBasket(objPizza));
-  }
+  };
 
   const onChangePageActive = (page) => {
     dispatch(setPageActive(page));
-  }
+  };
 
   //количество карточек на странице
   const totalPizzaBlockToPage = 4;
-  //итоговый массив пицц после поиска по названию, 
+  //итоговый массив пицц после поиска по названию,
   //так как фильтрация и поиск в mockapi совместно работают некорректно
-  const itemsResultToSearch = items.filter(item => item.name.toLowerCase().includes(searchName));
-  const itemsResultToPage = itemsResultToSearch.filter((item, index) => index >= pageActive*totalPizzaBlockToPage-totalPizzaBlockToPage && index < pageActive*totalPizzaBlockToPage);
+  const itemsResultToSearch = items.filter((item) => item.name.toLowerCase().includes(searchName));
+  const itemsResultToPage = itemsResultToSearch.filter(
+    (item, index) =>
+      index >= pageActive * totalPizzaBlockToPage - totalPizzaBlockToPage &&
+      index < pageActive * totalPizzaBlockToPage,
+  );
   //количетсво страниц
   const totalPages = Math.ceil(itemsResultToSearch.length / totalPizzaBlockToPage);
 
   return (
     <div className="container">
       <div className="content__top">
-        <Categories activeCategory={category} setActiveCategory={setActiveCategory}/>
-        <SortPopup activeSort={sortBy} setActiveSortBy={setActiveSortBy}/>
+        <Categories activeCategory={category} setActiveCategory={setActiveCategory} />
+        <SortPopup activeSort={sortBy} setActiveSortBy={setActiveSortBy} />
       </div>
 
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">
         {isLoading
-        ?
-          itemsResultToPage.map(item => 
-            <PizzaBlock key={item.id} basketItems={basketItems[item.id] && basketItems[item.id].items.length} onClickAddPizza={onClickAddPizza} {...item}/>
-          )
-        :
-            Array(12).fill(0).map((_, index) => <PizzaLoadingBlock key={index}/>)
-        }
+          ? itemsResultToPage.map((item) => (
+              <PizzaBlock
+                key={item.id}
+                basketItems={basketItems[item.id] && basketItems[item.id].items.length}
+                onClickAddPizza={onClickAddPizza}
+                {...item}
+              />
+            ))
+          : Array(12)
+              .fill(0)
+              .map((_, index) => <PizzaLoadingBlock key={index} />)}
       </div>
-      <Pagination 
+      <Pagination
         pageActive={pageActive}
         onChangePageActive={onChangePageActive}
         totalPages={totalPages}
       />
     </div>
   );
-}
+};
 
 export default Home;
